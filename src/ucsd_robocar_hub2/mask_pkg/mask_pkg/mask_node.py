4f2c2bc1bdf8428efab6311dcd451f471be91a80
Jian Qin <qinjianxyz@gmail.com>
2022-03-08 19:35:46 -0800

    fix amount processed

--- src/ucsd_robocar_hub2/mask_pkg/mask_pkg/mask_node.py
+++ src/ucsd_robocar_hub2/mask_pkg/mask_pkg/mask_node.py
@@ -28,13 +28,28 @@ class MaskDetection(Node):
         self.camera_subscriber = self.create_subscription(Image,
         CAMERA_TOPIC_NAME, self.detect_mask, 10)
         self.mask_publisher = self.create_publisher(Int32, MASK_DETECTION_TOPIC_NAME, 1)
+        self.timer_period = 2
+        self.timer = self.create_timer(self.timer_period, self.publish_data)
         self.servo_publisher = self.create_publisher(Float32,
         SERVO_TOPIC_NAME, 10)
         self.bridge = CvBridge()
         self.mask_detected = Int32()
         self.mask_degree = Float32()
+        self.counter = 0 # bad
+
+    def publish_data(self):
+        self.servo_publisher.publish(self.mask_degree)
+        self.mask_publisher.publish(self.mask_detected)
+
 
     def detect_mask(self, data):
+
+        self.counter += 1
+        if self.counter != 3:
+            return
+        else:
+            self.counter = 0
+
         frame = self.bridge.imgmsg_to_cv2(data)  # camera feed
 
         # detection process
@@ -52,13 +67,9 @@ class MaskDetection(Node):
             if label == 'without_mask' and score > 0.9:
                 self.mask_detected.data = 1
                 self.mask_degree.data = 0.0
-                self.servo_publisher.publish(self.mask_degree)
-                self.mask_publisher.publish(self.mask_detected)
             else:
-                self.mask_degree.data = 90.0
-                self.servo_publisher.publish(self.mask_degree)
                 self.mask_detected.data = 0
-                self.mask_publisher.publish(self.mask_detected)
+                self.mask_degree.data = 90.0
 
 
 def main(args=None):
